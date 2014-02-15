62bbd8490eaba98a4f6cb927df00a77a5e9d5a3b
DeadDork <nimrodomer@gmail.com>
2014-02-14 17:17:28 -0800

    Init: explores difference between `null` and `undefined`

---
+++ experiments/null_vs_undefined_1.js
@@ -0,0 +1,5 @@
+x = null; // a value that indicates a specific non-value.
+console.log("x = " + x);
+
+y = undefined; // a value that indicates the type of the object is undefined.
+console.log("y = " + y);
