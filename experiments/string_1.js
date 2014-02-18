bed5b5177fadd5ae6082581f0c69a14838589a6c
DeadDork <nimrodomer@gmail.com>
2014-02-18 00:23:57 -0800

    Init: explores the use of the substring string method

---
+++ experiments/string_1.js
@@ -0,0 +1,21 @@
+// Explores the substring string method.
+
+// Conclusion: substring works exactly as you would expect it to.
+
+////////////////////////////////////////////////////////////////////////////////
+// Functions
+
+function main() {
+	var string = "hello";
+	
+	print(string.substring(1));
+}
+
+function print(out) {
+	console.log(out);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Execute
+
+main();
