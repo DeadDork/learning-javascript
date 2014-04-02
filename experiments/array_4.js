3e8213c51a83c71b30445f230c0ef0a21fd6ffd3
DeadDork <nimrodomer@gmail.com>
2014-04-01 22:42:59 -0700

    Init: explores pushing multiple objects into an array

---
+++ experiments/array_4.js
@@ -0,0 +1,23 @@
+// Explores pushing multiple objects into an array.
+
+// Seems you can push multiple objects juuuuuust fine
+
+////////////////////////////////////////////////////////////////////////////////
+// Functions
+
+function main() {
+	var array = [];
+	print(array);
+
+	array.push(1, 2);
+	print(array);
+}
+
+function print(input) {
+	console.log(input);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Execute
+
+main();
