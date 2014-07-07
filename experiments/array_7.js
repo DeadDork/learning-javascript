4780e9ea29a75860339c3015cca8203a6dbdc209
DeadDork <nimrodomer@gmail.com>
2014-07-07 12:24:20 -0700

    Explores multidimensional insertions

--- experiments/array_7.js
+++ experiments/array_7.js
@@ -5,6 +5,8 @@ var a = [];
 //a[7] = [];
 //a[7].push([1,2]); // cannot call method push of undefined
 
-a[3] = [[1,2], 3];
+a[3] = [[1,2], 3]; // works
+
+//a[1][2] = [[4,5], 6]; // Cannot set property of undefined
 
 console.log(a);
