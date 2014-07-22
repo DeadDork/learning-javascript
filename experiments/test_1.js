16e8b99fad633dac977cd14e9548e872fd86348f
DeadDork <nimrodomer@gmail.com>
2014-07-21 22:20:40 -0700

    Init: tests an eval string (for a GAS project)

---
+++ experiments/test_1.js
@@ -0,0 +1,4 @@
+// Just checking that this prints out correctly.
+
+var i = 1;
+console.log('var widget' + i + ' = app.createTextBox().setReadOnly(readOnly).setName("textBox' + i + '").addClickHandler(handler)');
